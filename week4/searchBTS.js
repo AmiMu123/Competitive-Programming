ebceeeaa6d4844f8cf15c8285773cb6ee50710f3
Amir <amirmustefa707@gmail.com>
2021-01-21 19:21:02 +0300

    solved search in BST

---
+++ week4/searchBTS.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+   
+    let ans = search(root,val);
+    return ans ? ans : null;
+};
+
+let search = function(root,val){
+        if(root !==null){
+            if(val > root.val){
+             return  searchBST(root.right,val);
+                
+}
+            if(val< root.val){
+                return searchBST(root.left,val);
+            }
+            if(val === root.val){
+                return root;
+            }
+            
+        }
+  
+}
+
