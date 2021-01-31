f5e96d538e095ba6e2231fc44f16122fe98ba9da
Amir <amirmustefa707@gmail.com>
2021-01-31 09:43:05 +0300

    remove adjecent elements of a string

---
+++ week3/removeAdjecentDup.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var removeDuplicates = function(S) {
+    let array = []
+    for(let i = 0;i<S.length ; i++){
+        array.push(S[i])
+    }
+    let stack = [];
+    while(array.length !==0){
+        if(array[array.length-1] !== stack[stack.length-1]){
+            stack.push(array[array.length - 1]);
+            array.pop();
+            
+            
+        }
+       else if(array[array.length-1] === stack[stack.length-1]){
+           
+           stack.pop();
+           array.pop();
+       }
+    };
+    
+    return stack.reverse().join("");
+}
--- week3/removeLinkedListElement.js
+++ week3/removeLinkedListElement.js
@@ -24,7 +24,6 @@ var removeElements = function(head, val) {
              prev.next = curr.next.next;
    }
            else{
-           prev = prev.next;
            curr = curr.next;
    }
    }
@@ -32,38 +31,3 @@ var removeElements = function(head, val) {
       
    }
 
-
-
-
-   
-   // with only one pointer
-
-   /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @param {number} val
- * @return {ListNode}
- */
-var removeElements = function(head, val) {
-    while(head !==null && head.val ===val){
-       head = head.next;
-   }
-       let curr =head;
-   
-       while(curr!==null && curr.next !==null){
-           if(curr.next.val ===val){
-               curr.next = curr.next.next;
-           }
-           else{
-               curr = curr.next;
-   }
-       }
-       return head;
-      
-   }
