5e41eec8b6e871706b1d0c26a9e3746a85a8e8ad
Amir <amirmustefa707@gmail.com>
2021-02-24 19:33:06 +0300

    solved string compression

---
+++ practice/stringComp.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def compress(self, word):
+
+        count = 0
+        comp = ""
+        for i in range(len(word)):
+            count += 1
+
+            if len(word) == 1 or i+1 == len(word) or word[i] != word[i + 1]:
+
+                comp = comp + word[i]
+                comp += str(count) if count > 1 else ""
+                count = 0
+
+        for i, char in enumerate(comp):
+            word[i] = char
+        return len(comp)
