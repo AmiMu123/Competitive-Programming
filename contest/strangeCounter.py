b5faf9bf843b73bdf8f07df0ac7a9d31a238329a
Amir <amirmustefa707@gmail.com>
2021-02-25 18:17:56 +0300

    solved strange counter

--- contest/strangeCounter.py
+++ contest/strangeCounter.py
@@ -1,5 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the strangeCounter function below.
+
+
 def strangeCounter(t):
-    n = 3
-    while 2*n-2 <= t:
-        n *= 2
-    return n-(t-(n-2))
+    initValue = 3
+    while(t > initValue):
+        t = t - initValue
+        initValue = initValue * 2
+    return (initValue - t + 1)
+#     n=3
+#     while 2*n-2<=t:
+#         n*=2
+#     return n-(t-(n-2))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
