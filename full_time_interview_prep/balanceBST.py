9b5261fbb590cde6bf661272f4717385e35e3c18
AmirMustefa <32706438+AmirMustefa11@users.noreply.github.com>
2021-07-08 12:08:41 +0300

    solved balance binary tree

---
+++ full_time_interview_prep/balanceBST.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        arr = []
+        self.inorder(root,arr)
+        l = len(arr)
+        return self.constructBST(arr, 0, l)
+    
+    def constructBST(self, arr, start, end):
+        if start >= end:
+        
+            return None
+        
+        rootIndex = (start + end) // 2
+        
+        node = TreeNode(arr[rootIndex])
+        node.left = self.constructBST(arr, start, rootIndex)
+        node.right = self.constructBST(arr, rootIndex + 1, end)
+        return node
+
+    def inorder(self,node,arr):
+        if node.left:
+            self.inorder(node.left,arr)
+        arr.append(node.val)
+        if node.right:
+            self.inorder(node.right,arr)
