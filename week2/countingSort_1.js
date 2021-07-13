9fa68663f9ecd18c64e60de4acabe528059d9ac8
AmirMustefa <32706438+AmirMustefa11@users.noreply.github.com>
2021-07-14 00:23:23 +0300

    peak element

--- week2/countingSort_1.js
+++ week2/countingSort_1.js
@@ -1,14 +1,14 @@
 function countingSort(arr) {
-    let max = Math.max(...arr)+1
-    let container = new Array(max).fill(0) 
-    for(let i = 0;i<arr.length;i++){
+    let max = Math.max(...arr) + 1
+    let container = new Array(max).fill(0)
+    for (let i = 0; i < arr.length; i++) {
         let curr = arr[i];
-        container[curr] +=1;
+        container[curr] += 1;
     }
-    
-    let result= []
-    for(let i = 0;i<container.length;i++){
-        for(let j=0;j<container[i];j++){
+
+    let result = []
+    for (let i = 0; i < container.length; i++) {
+        for (let j = 0; j < container[i]; j++) {
             result.push(i);
         }
     }
---
+++ week2/palindromPer.py
@@ -0,0 +1,10 @@
+# bruteforce
+# -	Check if the string is odd or even 
+# -	If odd : every letter in the string have an even count , odd count =1
+# If even : every letter in the string even count
+
+
+
+
+
+
